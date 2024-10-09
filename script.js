8c9840a159d867e8da82657ef9040b5986fa8452
nastyaaawww <161913300+nastyaaawww@users.noreply.github.com>
2024-10-09 18:01:12 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+anime({
+   targets:'.wheel',
+   translateX:'250px',
+   rotate:360,
+   delay:1000,
+   duration:750,
+   easing:'linear'
+
+}).finished.then(function(){
+  anime({
+   targets:'.wheel',
+   translateX:'500px',
+   rotate:720,
+   delay:1000,
+   duration:750,
+   easing:'linear'
+    
+})
+
+
+})
